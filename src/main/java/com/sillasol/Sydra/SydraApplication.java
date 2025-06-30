28b16ca639b4599542c665a0036b2424ad56796c
DESKTOP-RRHE6I4\herme <hermes7308@gmail.com>
2025-06-30 22:09:47 +0900

    Initial commit

---
+++ src/main/java/com/sillasol/Sydra/SydraApplication.java
@@ -0,0 +1,13 @@
+package com.sillasol.Sydra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SydraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SydraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sillasol/Sydra/SydraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sillasol.Sydra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SydraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
